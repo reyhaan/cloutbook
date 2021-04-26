f4e022bfa468e659785b262b6c11a1e05d21610d
Mohammad Rehaan <frankfill2007@gmail.com>
2021-04-26 16:14:25 -0400

    update

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -24,7 +24,7 @@ class LoginScreen extends HookWidget {
     LoggedInUser? loggedInUser = _authStore.getLoggedInUser();
 
     if (loggedInUser != null) {
-      _profileStore.loggedInProfile = loggedInUser!.username;
+      _profileStore.loggedInProfile = loggedInUser.username;
       _authStore.setLoggedInUser(loggedInUser);
       AutoRouter.of(context).replace(NavRoute());
     }
